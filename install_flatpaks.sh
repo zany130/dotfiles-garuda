fe112e855b0e36f9d6975aa6806d1606fd4339ac
zany130 <andresdortiz@gmail.com>
2024-04-27 18:51:56 -0400

    add better flatpak list

---
+++ install_flatpaks.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+flatpak install com.google.Chrome com.leinardi.gst com.quexten.Goldwarden com.quexten.Goldwarden com.supermodel3.Supermodel com.usebottles.bottles io.github.achetagames.epic_asset_manager io.github.alainm23.planify io.github.hmlendea.geforcenow-electron io.github.shiiion.primehack org.kde.krdp re.chiaki.Chiaki xyz.tytanium.DoorKnocker
